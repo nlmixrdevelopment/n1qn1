0ca8734cad04ade3ed936755957e84d047268732
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-06-30 08:25:22 -0500

    Remove 'qnbd'

--- src/init.c
+++ src/init.c
@@ -20,19 +20,10 @@ void n1qn1F2(S2_fp simul, int n[], double x[], double f[], double g[], double va
 }
 /* .C calls */
 extern SEXP n1qn1_wrap(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP qnbd_wrap(SEXP, SEXP, SEXP, SEXP, SEXP, 
-		      SEXP, SEXP, SEXP, SEXP, SEXP, 
-		      SEXP, SEXP, SEXP, SEXP);
-
-extern void qnbd_(int* indqn, S2_fp fn, int* n, double* x, double* f, double* g, int* iprint, double* zero, int* napmax, 
-		  int* itmax, double* epsf, double* epsg, double* epsx, double* df0, 
-		  double* binf, double* binsup, int* nfac, double* trav, int* ntrav, int* itrav, int* nitrav, 
-		  int* izs, float* rzs, double* dzs);
 
 
 static const R_CallMethodDef CallEntries[] = {
   {"n1qn1_wrap", (DL_FUNC) &n1qn1_wrap, 13},
-  {"qnbd_wrap", (DL_FUNC) &qnbd_wrap, 14},
   {NULL, NULL, 0}
 };
 
@@ -40,7 +31,6 @@ void R_init_n1qn1(DllInfo *dll)
 {
   R_RegisterCCallable("n1qn1","n1qn1F", (DL_FUNC) n1qn1F);
   R_RegisterCCallable("n1qn1","n1qn1F2", (DL_FUNC) n1qn1F2);
-  R_RegisterCCallable("n1qn1","qnbdF", (DL_FUNC) qnbd_);
   R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
   R_useDynamicSymbols(dll, TRUE);
   R_forceSymbols(dll,TRUE);
